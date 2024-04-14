3f26fe20cef42b8da843c1a0d458c00c7d992f7f
Juan Bautista Valero Carrasco <jbvc91@gmail.com>
2024-04-14 18:12:30 +0200

    Se ha añadido una paginación más avanzada y se ha refactorizado el código

--- src/departments/DepartmentsPage.tsx
+++ src/departments/DepartmentsPage.tsx
@@ -11,78 +11,6 @@ function DepartmentsPage() {
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(5);
 
-  const saveDepartment = async (department: Department) => {
-    let updatedDepartments = departments.map((u: Department) => {
-      return u.id === department.id ? department : u;
-    });
-    setDepartments(updatedDepartments);
-    // Actualiza el usuario en la base de datos
-    const response = await fetch('http://127.0.0.1:8081/api/rpc', {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        "id": 1,
-        "method": "update_department",
-        "params": {
-          "id": department.id,
-          "data": {
-            "name": department.name,
-          }
-        }
-      }),
-    });
-
-    if (!response.ok) {
-      console.error(`Error al actualizar el usuario`);
-    }
-  };
-
-  const createDepartment = async (department: Department) => {
-    // Actualiza el usuario en la base de datos
-    const response = await fetch('http://127.0.0.1:8081/api/rpc', {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        "id": 1,
-        "method": "create_department",
-        "params": {
-          "data": {
-            "name": department.name,
-          }
-        }
-      }),
-    });
-
-    if (!response.ok) {
-      console.error(`Error al actualizar el usuario`);
-    }
-    if (departments.length === itemsPerPage) {
-      setCurrentPage(currentPage + 1);
-    }
-    fetchAllData();
-  };
-
-  const handleDeleteDepartment = async (department: Department) => {
-    try{
-      await fetchData("delete_department", { "id": department.id });
-
-      if (departments.length === 1) {
-        setCurrentPage(currentPage - 1);
-      }
-      fetchAllData();  
-    } catch (error){
-      if(error instanceof Error)
-      alert(error.message);
-      alert("No se puede borrar el departamento porque hay docentes que pertenecen a el");
-    }
-  };
-
   const fetchData = async (method: string, params: object) => {
     const response = await fetch('http://127.0.0.1:8081/api/rpc', {
       method: 'POST',
@@ -106,25 +34,7 @@ function DepartmentsPage() {
     }
   };
 
-  const checkLogin = () => {
-    const miCookie = Cookies.get('loged_in');
-    if (miCookie !== "true") {
-      navigate("/login");
-    }
-  };
-
-  const fetchAllData = async () => {
-    const allDepartmentss = await fetchData("list_departments", {
-      "filters": {
-        "id": { "$gte": 1000 }
-      },
-      "list_options": {
-        "order_bys": "id"
-      }
-    });
-    if (allDepartmentss) {
-      setTotalDepartments(allDepartmentss.length);
-    }
+  async function fetchDepartments() {
     const departmentsResponse = await fetchData("list_departments", {
       "filters": {
         "id": { "$gte": 1000 }
@@ -136,22 +46,35 @@ function DepartmentsPage() {
       }
     });
     if (departmentsResponse) {
-      setDepartments(departmentsResponse)
+      setDepartments(departmentsResponse);
     }
   }
 
-  const checkPage = (fromCreate: boolean) => {
+  async function fecthAllDepartments() {
+    const allDepartmentss = await fetchData("list_departments", {
+      "filters": {
+        "id": { "$gte": 1000 }
+      },
+      "list_options": {
+        "order_bys": "id"
+      }
+    });
 
-    
+    if (allDepartmentss) {
+      setTotalDepartments(allDepartmentss.length);
+    }
+  }
+  
+  const fetchAllData = async () => {
+    await fecthAllDepartments();
+    await fetchDepartments();
   }
 
-  useEffect(() => {
-    checkLogin();
-    fetchAllData();
-  }, [currentPage, itemsPerPage]);
+  const handlePagination = (newPage: number) => {
+    setCurrentPage(newPage);
+  };
 
-
-  const handleChangeItems = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleItemsPerPageChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const n = Number(event.target.value);
     setItemsPerPage(n);
     if (n == 0 || n > totalDepartments)
@@ -163,38 +86,109 @@ function DepartmentsPage() {
     }
   };
 
-  const totalPages = Math.ceil(totalDepartments / itemsPerPage);
+  const handleCreateOrUpdateDepartment = async (department: Department) => {
+    const update = department.id ? true : false;
+    const method = update ? "update_department" : "create_department";
+    const data = update ? { "id": department.id, "data": { "name": department.name } } : { "data": { "name": department.name } };
 
+    const responseData = await fetchData(method, data);
+
+    if (!responseData.id ? true : false) {
+      console.error(`Error al ${method === "update_department" ? "actualizar" : "crear"} el departamento`);
+    }
+    if (departments.length === itemsPerPage) {
+      setCurrentPage(currentPage + 1);
+    }
+    if (update) {
+      let updatedDepartments = departments.map((u: Department) => {
+        return u.id === department.id ? department : u;
+      });
+      setDepartments(updatedDepartments);
+    } else {
+      fetchAllData();
+    }
+  };
+
+  const handleDeleteDepartment = async (department: Department) => {
+    try {
+      await fetchData("delete_department", { "id": department.id });
+
+      if (departments.length === 1) {
+        setCurrentPage(currentPage - 1);
+      }
+      fetchAllData();
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message);
+      }
+      alert("No se puede borrar el departamento porque hay docentes que pertenecen a �l");
+    }
+  };
+
+  const checkLogin = () => {
+    const miCookie = Cookies.get('loged_in');
+    if (miCookie !== "true") {
+      navigate("/login");
+    }
+  };
+
+  useEffect(() => {
+    checkLogin();
+    fetchAllData();
+  }, [currentPage, itemsPerPage]);
+
+  const totalPages = Math.ceil(totalDepartments / itemsPerPage);
+  const paginationRange = 9;
+  const halfPaginationRange = Math.floor(paginationRange / 2);
+  const startPage = Math.max(1, currentPage - halfPaginationRange);
+  const endPage = Math.min(totalPages, startPage + paginationRange - 1);
 
   return (
     <div className="items-center justify-center bg-gray-500">
       <div className='p-8 pt-auto text-3xl font-semibold text-gray-800'>
         <h1>Departamentos</h1>
       </div>
-      <DepartmentList onCreate={createDepartment} onSave={saveDepartment} departments={departments} onDelete={handleDeleteDepartment} />
+      <DepartmentList onCreate={handleCreateOrUpdateDepartment} onSave={handleCreateOrUpdateDepartment} departments={departments} onDelete={handleDeleteDepartment} />
       <div className="flex items-center justify-center space-x-4">
-        <select value={itemsPerPage} onChange={handleChangeItems}>
-          <option value={5}>5</option>
-          <option value={10}>10</option>
-          <option value={20}>20</option>
-          <option value={50}>50</option>
-          <option value={0}>Todos</option>
+        <select value={itemsPerPage} onChange={handleItemsPerPageChange}>
+          {[5, 10, 20, 50, 0].map((value, index) => (
+            <option key={index} value={value}>{value === 0 ? "Todos" : value}</option>
+          ))}
         </select>
         <button
-          onClick={() => setCurrentPage(currentPage - 1)}
+          onClick={() => handlePagination(1)}
+          disabled={currentPage === 1 || itemsPerPage === 0}
+          className={`px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none ${currentPage === 1 && 'opacity-50 cursor-not-allowed'}`}>
+          Primera
+        </button>
+        <button
+          onClick={() => handlePagination(currentPage - 1)}
           disabled={currentPage === 1 || itemsPerPage === 0}
           className={`px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none ${currentPage === 1 && 'opacity-50 cursor-not-allowed'}`}>
           Anterior
         </button>
-        <span className="text-lg font-bold">Página {currentPage}</span>
+        {Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i).map(page => (
+          <button
+            key={page}
+            onClick={() => handlePagination(page)}
+            className={`px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none ${currentPage === page && 'bg-blue-700'}`}>
+            {page}
+          </button>
+        ))}
         <button
-          onClick={() => setCurrentPage(currentPage + 1)}
+          onClick={() => handlePagination(currentPage + 1)}
           disabled={currentPage === totalPages || itemsPerPage === 0}
           className={`px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none ${(currentPage === totalPages || itemsPerPage === 0) && 'opacity-50 cursor-not-allowed'}`}>
           Siguiente
         </button>
-      </div>
+        <button
+          onClick={() => handlePagination(totalPages)}
+          disabled={currentPage === totalPages || itemsPerPage === 0}
+          className={`px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none ${(currentPage === totalPages || itemsPerPage === 0) && 'opacity-50 cursor-not-allowed'}`}>
+          Última
+        </button>
 
+      </div>
     </div>
   );
 }
